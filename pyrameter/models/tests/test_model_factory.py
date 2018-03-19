16322ef43a0facd4c74afcbd3e95c9b041afbe33
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-03-19 14:11:48 -0400

    add tests for getting invalid models from model_factory

--- pyrameter/models/tests/test_model_factory.py
+++ pyrameter/models/tests/test_model_factory.py
@@ -1,7 +1,8 @@
 import pytest
 
-from pyrameter.models import get_model_class
-from pyrameter.models import RandomSearchModel, TPEModel, GPBayesModel
+from pyrameter.models import get_model_class, RandomSearchModel, TPEModel, \
+                             GPBayesModel
+from pyrameter.models.model_factory import InvalidModelError
 
 
 def test_get_model_class():
@@ -14,3 +15,8 @@ def test_get_model_class():
     for key, val in model_classes.items():
         assert get_model_class(key) is val
         assert get_model_class(val()) is val
+
+    invalids = [None, True, False, 1, 1.0, '1', [], {}, tuple(), set()]
+    for i in invalids:
+        with pytest.raises(InvalidModelError):
+            get_model_class(i)
