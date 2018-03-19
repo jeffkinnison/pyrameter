469daa6dae45dce8d601268968b47af3e15b6dde
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-03-19 14:28:07 -0400

    add test scaffolding for storage classes

---
+++ pyrameter/db/tests/test_local.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pyrameter.db import JsonStorage
+
+
+class TestJsonStorage(object):
+    def test_init(self):
+        pass
+
+    def test_load(self):
+        pass
+
+    def test_save(self):
+        pass
---
+++ pyrameter/db/tests/test_mongo.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pyrameter.db import MongoStorage
+
+
+class TestMongoStorage(object):
+    def test_init(self):
+        pass
+
+    def test_load(self):
+        pass
+
+    def test_save(self):
+        pass
