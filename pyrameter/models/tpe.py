65d56d09eea527ec4ad4530d83b1cf9d48fce928
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-04-09 20:29:09 -0400

    make TPE conform with the algorithm

--- pyrameter/models/tpe.py
+++ pyrameter/models/tpe.py
@@ -25,25 +25,29 @@ class TPEModel(Model):
         idx = np.argsort(losses, axis=0)
         split = int(np.ceil(idx.shape[0] * self.best_split))
         losses = np.reshape(losses, (-1, 1))
-        self.l.fit(features[idx[:split]], losses[idx[:split]])
-        self.g.fit(features[idx[split:]], losses[idx[split:]])
 
-        samples = self.l.sample(n_samples=10)
-        score_l = self.l.score(samples)
-        score_g = self.g.score(samples)
+        for j in range(features.shape[1]):
+            self.l.fit(features[np.reshape(idx[:split, j]], (-1, 1)),
+                       losses[idx[:split]])
+            self.g.fit(features[np.reshape(idx[split:, j]], (-1, 1)),
+                       losses[idx[split:]])
 
-        ei = score_l / score_g
-        best = samples[np.argmax(ei)]
+            samples = self.l.sample(n_samples=10)
+            score_l = self.l.score(samples)
+            score_g = self.g.score(samples)
 
-        params = {}
-        for i in range(len(self.domains)):
-            domain = self.domains[i]
-            path = domain.path.split('/')
-            curr = params
-            for p in path[:-1]:
-                if p not in curr:
-                    curr[p] = {}
-                curr = curr[p]
-            curr[path[-1]] = domain.map_to_domain(samples[i])
+            ei = score_l / score_g
+            best = samples[np.argmax(ei)]
+
+            params = {}
+            for i in range(len(self.domains)):
+                domain = self.domains[i]
+                path = domain.path.split('/')
+                curr = params
+                for p in path[:-1]:
+                    if p not in curr:
+                        curr[p] = {}
+                    curr = curr[p]
+                curr[path[-1]] = domain.map_to_domain(samples[i])
 
         return params
