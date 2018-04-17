2022f809cf90c4b5fb7f851e0bbec1e24d7f6ac0
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-04-17 19:47:15 -0400

    add warm up random search

--- pyrameter/models/tpe.py
+++ pyrameter/models/tpe.py
@@ -1,53 +1,58 @@
-from pyrameter.models.model import Model
+from pyrameter.models.random_search import RandomSearchModel
 
 import numpy as np
 from sklearn.mixture import GaussianMixture
 
 
-class TPEModel(Model):
+class TPEModel(RandomSearchModel):
     def __init__(self, id=None, domains=None, results=None,
                  update_complexity=True, priority_update_freq=10,
-                 best_split=0.2, n_samples=10, **gmm_kws):
+                 best_split=0.2, n_samples=10, warm_up=10, **gmm_kws):
         super(TPEModel, self).__init__(id=id,
                                        domains=domains,
                                        results=results,
                                        update_complexity=update_complexity,
                                        priority_update_freq= \
                                             priority_update_freq)
-        self.l = GaussianMixture(**gmm_kws)
-        self.g = GaussianMixture(**gmm_kws)
+        self.gmm_kws = gmm_kws
         self.best_split = best_split
         self.n_samples = n_samples
+        self.warm_up = warm_up
 
     def generate(self):
-        vec = self.__results_to_feature_vector()
-        features, losses = np.copy(vec[:, :-1]), np.copy(vec[:, -1])
-        idx = np.argsort(losses, axis=0)
-        split = int(np.ceil(idx.shape[0] * self.best_split))
-        losses = np.reshape(losses, (-1, 1))
+        if len(self.results) < self.warm_up:
+            params = super(TPEModel, self).generate()
+        else:
+            vec = self.__results_to_feature_vector()
+            features, losses = np.copy(vec[:, :-1]), np.copy(vec[:, -1])
+            idx = np.argsort(losses, axis=0)
+            split = int(np.ceil(idx.shape[0] * self.best_split))
+            losses = np.reshape(losses, (-1, 1))
 
-        for j in range(features.shape[1]):
-            self.l.fit(features[np.reshape(idx[:split, j], (-1, 1))],
-                       losses[idx[:split]])
-            self.g.fit(features[np.reshape(idx[split:, j], (-1, 1))],
-                       losses[idx[split:]])
+            for j in range(features.shape[1]):
+                l = GaussianMixture(**self.gmm_kws)
+                g = GaussianMixture(**self.gmm_kws)
+                self.l.fit(features[np.reshape(idx[:split, j], (-1, 1))],
+                           losses[idx[:split]])
+                self.g.fit(features[np.reshape(idx[split:, j], (-1, 1))],
+                           losses[idx[split:]])
 
-            samples = self.l.sample(n_samples=10)
-            score_l = self.l.score(samples)
-            score_g = self.g.score(samples)
+                samples = l.sample(n_samples=10)
+                score_l = l.score(samples)
+                score_g = g.score(samples)
 
-            ei = score_l / score_g
-            best = samples[np.argmax(ei)]
+                ei = score_l / score_g
+                best = samples[np.argmax(ei)]
 
-            params = {}
-            for i in range(len(self.domains)):
-                domain = self.domains[i]
-                path = domain.path.split('/')
-                curr = params
-                for p in path[:-1]:
-                    if p not in curr:
-                        curr[p] = {}
-                    curr = curr[p]
-                curr[path[-1]] = domain.map_to_domain(samples[i])
+                params = {}
+                for i in range(len(self.domains)):
+                    domain = self.domains[i]
+                    path = domain.path.split('/')
+                    curr = params
+                    for p in path[:-1]:
+                        if p not in curr:
+                            curr[p] = {}
+                        curr = curr[p]
+                    curr[path[-1]] = domain.map_to_domain(samples[i])
 
         return params
