c89cc0f8f2b5b8bc25a8ba7ed85a66ee2bf56189
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-04-18 12:26:47 -0400

    test TPEModel.generate

--- pyrameter/models/tests/test_tpe.py
+++ pyrameter/models/tests/test_tpe.py
@@ -2,6 +2,10 @@ import pytest
 from test_model import TestModel
 
 from pyrameter.models import TPEModel
+from pyrameter.models.model import Result, Value
+from pyrameter import ContinuousDomain, DiscreteDomain
+
+from scipy.stats import uniform
 
 
 class TestTPEModel(TestModel):
@@ -26,4 +30,34 @@ class TestTPEModel(TestModel):
         super(TestTPEModel, self).test_init()
 
     def test_generate(self):
-        pass
+        d1 = ContinuousDomain(uniform, path='a', loc=-100.0, scale=100.0)
+        d2 = DiscreteDomain([i for i in range(-100, 0)] +
+                            [i for i in range(1, 101)], path='b')
+
+        # Test with one continuous domain
+        m = self.__model_class__(domains=[d1])
+        for _ in range(1000):
+            p = m.generate()
+            m.add_result(Result(m, loss=p['a']**2, values=Value(p['a'], d1)))
+            assert 'a' in p
+            assert p['a'] >= -100.0 and p['a'] < 100
+
+        # Test with one continuous domain
+        m = self.__model_class__(domains=[d2])
+        for _ in range(1000):
+            p = m.generate()
+            m.add_result(Result(m, loss=p['b']**2, values=Value(p['b'], d2)))
+            assert 'b' in p
+            assert p['b'] >= -100 and p['b'] <= 100
+
+        # Test with one continuous and one discrete domain
+        m = self.__model_class__(domains=[d1, d2])
+        for _ in range(1000):
+            p = m.generate()
+            print(p)
+            m.add_result(Result(m, loss=p['a'] * p['b'],
+                                values=[Value(p['a'], d1), Value(p['b'], d2)]))
+            assert 'a' in p
+            # assert p['a'] >= -100 and p['a'] < 100
+            assert 'b' in p
+            assert p['b'] >= -100 and p['b'] <= 100
