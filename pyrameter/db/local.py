9523227dea012683c0c7f30c9c37f3f23c064519
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-05-09 16:52:09 -0400

    set up local storage __init__

--- pyrameter/db/local.py
+++ pyrameter/db/local.py
@@ -1,10 +1,18 @@
 import json
+import os
 
 
 class JsonStorage(object):
     def __init__(self, path, keep_previous=1):
-        self.path = path
-        self.backups = backups
+        self.path = os.path.abspath(path)
+
+        if os.path.isdir(self.path):
+            self.path = os.path.join(self.path, 'results.json')
+
+        if not os.path.exists(os.path.dirname(self.path)):
+            raise OSError('Invalid save path: {}'.format(self.path))
+
+        self.backups = keep_previous
 
     def load(self):
         with open(self.path, 'r') as f:
