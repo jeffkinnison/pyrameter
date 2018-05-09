479ab745c95111554f31939fcee680b0d20dbd46
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-05-09 16:52:32 -0400

    tests for JsonStorage initialization

--- pyrameter/db/tests/test_local.py
+++ pyrameter/db/tests/test_local.py
@@ -1,14 +1,31 @@
 import pytest
 
-from pyrameter.db import JsonStorage
+from pyrameter.db.local import JsonStorage
+
+import os
 
 
 class TestJsonStorage(object):
-    def test_init(self):
+    def test_init(self, tmpdir):
+        # Test default instantiation
+        s = JsonStorage(tmpdir.strpath)
+        assert s.path == os.path.join(tmpdir.strpath, 'results.json')
+        assert s.backups == 1
+
+        # Test with supplied filename
+        s = JsonStorage(os.path.join(tmpdir.strpath, 'foo.json'), keep_previous=5)
+        assert s.path == os.path.join(tmpdir.strpath, 'foo.json')
+        assert s.backups == 5
+
+        # Test with nonexistent path
+        with pytest.raises(OSError):
+            JsonStorage('/foo/bar')
+
+        with pytest.raises(OSError):
+            JsonStorage('/foo/bar/baz.json')
+
+    def test_load(self, tmpdir):
         pass
 
-    def test_load(self):
-        pass
-
-    def test_save(self):
+    def test_save(self, tmpdir):
         pass
