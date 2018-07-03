471e7aa74a60e94fa3999d6c383ad83770eac630
Jeff Kinnison <jeffdkinnison@gmail.com>
2018-07-03 14:00:57 -0400

    add ModelGroup as package-level import

--- pyrameter/__init__.py
+++ pyrameter/__init__.py
@@ -1,5 +1,6 @@
 from .build import build
 from .scope import Scope
 from .domain import ContinuousDomain, DiscreteDomain, ExhaustiveDomain
+from .modelgroup import ModelGroup
 
 __all___ = ['Scope', 'ContinuousDomain', 'DiscreteDomain', 'ExhaustiveDomain']
