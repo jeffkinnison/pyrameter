3a8abe6f1754672c0572b44ed77825513ac398c6
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-02-19 11:07:07 -0500

    update some documentation

--- pyrameter/db/entities/domain.py
+++ pyrameter/db/entities/domain.py
@@ -3,7 +3,7 @@
 Classes
 -------
 Domain
-
+    Data model of a hyperparameter search domain.
 """
 
 from pyrameter.db.entities.entity import Entity
@@ -18,6 +18,8 @@ class Domain(Entity):
         Values in the hyperparameter domain.
     path : str
         The path to this domain in the hyperparameter tree.
+    complexity : float
+        The size of this hyperparameter domain.
 
     """
 
@@ -25,6 +27,79 @@ class Domain(Entity):
         super(Domain, self).__init__()
         self.domain = domain
         self.path = path
+        self.__complexity = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.generate()
+
+    def next(self):
+        return self.__next__()
+
+    def __eq__(self, other):
+        return self.to_json() == other.to_json()
+
+    def __str__(self):
+        return str(self.to_json())
+
+    @property
+    def complexity(self):
+        """Approximate the size of this domain.
+
+        The size of a continuous domain is approximated by computing the
+        magnitude of the interval containing 99%% of the distribution.
+
+        Notes
+        -----
+        This property implements the complexity formula for continuous domains
+        introduced by Kinnison *et al.* _[1]
+
+        References
+        ----------
+        ..  [1] Kinnison, J., Kremer-Herman, N., Thain, D., & Scheirer, W.
+            (2017). SHADHO: Massively Scalable Hardware-Aware Distributed
+            Hyperparameter Optimization. arXiv preprint arXiv:1707.01428.
+        """
+        raise NotImplementedError
+
+    def generate(self):
+        """Draw a value from this hyperparameter domain."""
+        raise NotImplementedError
+
+    def map_to_domain(self, idx, bound=False):
+        """Map a index to its value in the domain.
+
+        Parameters
+        ----------
+        idx : int
+            The index to retrieve in the domain.
+        bound : bool
+            If True, return the first or last element of the domain if ``idx``
+            < 0 or idx > |domain|, respectively.
+
+        Returns
+        -------
+        The value at ``idx`` in the domain if the domain is discrete, else
+        return the index.
+        """
+        return idx
+
+    def map_to_index(self, value):
+        """Map a value to its index in the domain.
+
+        Parameters
+        ----------
+        value
+            The value to find in the domain.
+
+        Returns
+        -------
+        The index of ``value`` in the domain if the domain is discrete, else
+        return the value.
+        """
+        return value
 
     def to_json(self):
         """Serialize this entity as JSON.
@@ -36,5 +111,6 @@ class Domain(Entity):
         """
         return {
             'domain': self.domain,
-            'path': self.path
+            'path': self.path,
+            'complexity': self.__complexity
         }
