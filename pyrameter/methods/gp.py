326a9833729534ec2bf75d8c0ae38105843753eb
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-05-06 13:14:18 -0400

    updated methods and clean up remaining Model interface

--- pyrameter/methods/gp.py
+++ pyrameter/methods/gp.py
@@ -1,71 +1,135 @@
-from pyrameter.models.random_search import RandomSearchModel
+"""Spearmint-style Bayesian optimization for hyperparameter optimization.
+
+Functions
+---------
+bayesian_optimization
+    Generate hyperparameters using Gaussian process Bayesian optimization.
+"""
 
 import numpy as np
 from scipy.stats import norm
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF
 
+from pyrameter.methods.random_search import random_search
 
-class GPBayesModel(RandomSearchModel):
-    """Gaussian process-based hyperparameter optimizer.
+
+def bayesian_optimization(search_space, n_samples=10, warm_up=10, **gp_kws):
+    """Generate hyperparameters using Gaussian process Bayesian optimization.
 
     Based on the Spearmint
 
     Paramaters
     ----------
-    id :
+    search_space : pyrameter.db.SearchSpace
+        The search space to draw values from.
+    n_samples : int, optional
+        The number of samples to generate from the l mixture model.
+        Default 10.
+    warm_up : int, optional
+        The number of iterations of random search to run before starting TPE.
+        Default 10.
+
+    Other Parameters
+    ----------------
+    **gp_kws
+        Additional keyword arguments for Gaussian mixture model hyperparameters
+
+    Returns
+    -------
+    hyperparameters : dict
+        The set of hyperparameters generated from this search space.
     """
+    n_results = len([r for r in search_space.results if r.loss is not None])
+    if n_results < warm_up or n_results % warm_up == 0:
+        hyperparameters = random_search(search_space)
+    else:
+        vec = search_space.results_vector()
+        features, losses = np.copy(vec[:, :-1]), np.copy(vec[:, -1])
+        losses = np.reshape(losses, (-1, 1))
 
-    TYPE = 'gp'
+        gp = GaussianProcessRegressor(**gp_kws)
+        gp.fit(features, losses)
 
-    def __init__(self, id=None, domains=None, results=None,
-                 update_complexity=True, priority_update_freq=10, n_samples=10,
-                 warm_up=10, **gp_kws):
-        super(GPBayesModel, self).__init__(id=id,
-                                           domains=domains,
-                                           results=results,
-                                           update_complexity=update_complexity,
-                                           priority_update_freq= \
-                                                priority_update_freq)
-        self.n_samples = n_samples
-        self.warm_up = warm_up
-        self.gp_kws = gp_kws
-        if 'kernel' not in self.gp_kws:
-            self.gp_kws['kernel'] = RBF()
+        potentials = None
+        for i in range(n_samples):
+            vals = random_search(search_space)
+            if potentials is None:
+                potentials = np.float(())
+            potentials[i] += vals
 
-    def generate(self):
-        if len(self.results) < self.warm_up or len(self.results) % self.warm_up == 0:
-            params = super(GPBayesModel, self).generate()
-        else:
-            vec = self.results_to_feature_vector()
-            features, losses = np.copy(vec[:, :-1]), np.copy(vec[:, -1])
-            #features = features.T
-            losses = np.reshape(losses, (-1, 1))
+        mu, sigma = gp.predict(potentials, return_std=True)
+        best = np.min(losses)
+        with np.errstate(divide='ignore'):
+            gamma = (mu - best) / sigma
+        ei = (mu - gamma) * norm.cdf(gamma) + sigma * norm.pdf(gamma)
+        ei[sigma == 0] = 0
 
-            gp = GaussianProcessRegressor(**self.gp_kws)
-            gp.fit(features, losses)
+        hyperparameters = potentials[np.argmax(ei, axis=1)]
 
-            potentials = np.zeros((self.n_samples, len(self.domains)))
-            for i in range(self.n_samples):
-                for j in range(len(self.domains)):
-                    val = self.domains[j].generate(index=True)
-                    if isinstance(val, tuple):
-                        val = val[1]
-                    potentials[i, j] += val
+    return hyperparameters
 
-            mu, sigma = gp.predict(potentials, return_std=True)
-            best = np.min(losses)
-            with np.errstate(divide='ignore'):
-                gamma = (mu - best) / sigma
-            ei = (mu - gamma) * norm.cdf(gamma) + sigma * norm.pdf(gamma)
-            ei[sigma == 0] = 0
 
-            best = potentials[np.argmax(ei, axis=1)]
-
-            params = np.zeros((len(self.domains),))
-            for i in range(len(self.domains)):
-                domain = self.domains[i]
-                params[i] += domain.map_to_domain(best[i][0],
-                                                  bound=True)
-
-        return params
+# class GPBayesModel(RandomSearchModel):
+#     """Gaussian process-based hyperparameter optimizer.
+#
+#     Based on the Spearmint
+#
+#     Paramaters
+#     ----------
+#     id :
+#     """
+#
+#     TYPE = 'gp'
+#
+#     def __init__(self, id=None, domains=None, results=None,
+#                  update_complexity=True, priority_update_freq=10, n_samples=10,
+#                  warm_up=10, **gp_kws):
+#         super(GPBayesModel, self).__init__(id=id,
+#                                            domains=domains,
+#                                            results=results,
+#                                            update_complexity=update_complexity,
+#                                            priority_update_freq= \
+#                                                 priority_update_freq)
+#         self.n_samples = n_samples
+#         self.warm_up = warm_up
+#         self.gp_kws = gp_kws
+#         if 'kernel' not in self.gp_kws:
+#             self.gp_kws['kernel'] = RBF()
+#
+#     def generate(self):
+#         if len(self.results) < self.warm_up or len(self.results) % self.warm_up == 0:
+#             params = super(GPBayesModel, self).generate()
+#         else:
+#             vec = self.results_to_feature_vector()
+#             features, losses = np.copy(vec[:, :-1]), np.copy(vec[:, -1])
+#             #features = features.T
+#             losses = np.reshape(losses, (-1, 1))
+#
+#             gp = GaussianProcessRegressor(**self.gp_kws)
+#             gp.fit(features, losses)
+#
+#             potentials = np.zeros((self.n_samples, len(self.domains)))
+#             for i in range(self.n_samples):
+#                 for j in range(len(self.domains)):
+#                     val = self.domains[j].generate(index=True)
+#                     if isinstance(val, tuple):
+#                         val = val[1]
+#                     potentials[i, j] += val
+#
+#             mu, sigma = gp.predict(potentials, return_std=True)
+#             best = np.min(losses)
+#             with np.errstate(divide='ignore'):
+#                 gamma = (mu - best) / sigma
+#             ei = (mu - gamma) * norm.cdf(gamma) + sigma * norm.pdf(gamma)
+#             ei[sigma == 0] = 0
+#
+#             best = potentials[np.argmax(ei, axis=1)]
+#
+#             params = np.zeros((len(self.domains),))
+#             for i in range(len(self.domains)):
+#                 domain = self.domains[i]
+#                 params[i] += domain.map_to_domain(best[i][0],
+#                                                   bound=True)
+#
+#         return params
--- pyrameter/methods/random_search.py
+++ pyrameter/methods/random_search.py
@@ -1,71 +1,23 @@
-from pyrameter.models.model import Model
+"""Random search for hyperparameter optimization.
+
+Functions
+---------
+random_search
+    Randomly generate hyperparameter values.
+"""
 
 
-class RandomSearchModel(Model):
-    """Generate hyperparameters for a model via random search.
+def random_search(search_space):
+    """Randomly generate hyperparameter values.
 
     Parameters
     ----------
-    id : str, optional
-        The id of this model. If not supplied, an id will be generated.
-    domains : list of ``pyrameter.Domain``, optional
-        The domains contained within this model.
-    results : list of ``pyrameter.models.Result``, optional
-        The results observed from evaluting different hyperparameterizations of
-        this model.
-    update_complexity : bool, optional
-        Whether to update model complexity when adding a new domain. Enabled by
-        default.
-    priority_update_freq : int, optional
-        How often the priority heuristic is updated, based on the number of
-        results. Values less than or equal to 0 disable priority updates.
-        Default: update every 10 results.
+    search_space : pyrameter.db.SearchSpace
+        The search space to draw values from.
 
-    Notes
-    -----
-    Random search as a viable method for hyperparameter optimization was
-    introduced by Bergstra and Bengio [1]_ as an alternative to grid search.
-
-    The complexity and priority heuristics are computed as described by
-    Kinnison *et al.* [2]_ .
-
-    References
-    ----------
-    .. [1] Bergstra, J., & Bengio, Y. (2012). Random search for hyper-parameter
-       optimization. Journal of Machine Learning Research, 13(Feb), 281-305.
-
-    .. [2] Kinnison, J., Kremer-Herman, N., Thain, D., & Scheirer, W. (2017).
-       SHADHO: Massively Scalable Hardware-Aware Distributed Hyperparameter
-       Optimization. arXiv preprint arXiv:1707.01428.
+    Returns
+    -------
+    hyperparameters : dict
+        The set of hyperparameters generated from this search space.
     """
-
-    TYPE = 'random'
-
-    def generate(self):
-        """Generate hyperparameter values.
-
-        Randomly generates hyperparameter values from each domain in this
-        model.
-
-        Returns
-        -------
-        A dictionary containing hyperparameter values structured the same as
-        the specification tree that created this model.
-
-        Notes
-        -----
-        This method implements random search as described by Bergstra and
-        Bengio [1]_ . Subclasses should be override this to implement different
-        hyperparameter generation methodologies.
-
-        References
-        ----------
-        .. [1] Bergstra, J., & Bengio, Y. (2012). Random search for
-           hyper-parameter optimization. Journal of Machine Learning Research,
-           13(Feb), 281-305.
-        """
-        # Create a list of randomly-generated paramters from each domain
-        params = []
-        for domain in self.domains:
-            params.append(domain.generate())
-        return params
+    return search_space.generate()
