190c0260a23e6e7824b801cd9a7f44572fb790ce
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-05-06 13:08:44 -0400

    update domain imports

---
+++ pyrameter/domains/__init__.py
@@ -0,0 +1,5 @@
+from .constant import ConstantDomain
+from .continuous import ContinuousDomain
+from .discrete import DiscreteDomain
+from .linked import LinkedDomain
+from .sequential import SequentialDomain
