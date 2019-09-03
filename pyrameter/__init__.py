e4b95f3b2ec1671ad0be71eb0754b6bcbb72d6a9
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-09-03 11:17:19 -0500

    include domains and helpers in top-level imports

--- pyrameter/__init__.py
+++ pyrameter/__init__.py
@@ -1,5 +1,12 @@
 """Structured hyperparameter optimization with flexible methods and storage."""
 
 from .optimizer import FMin
+from .domains import *
 
-__all__ = ['FMin']
+
+def uniform(loc, scale):
+    return ContinuousDomain('uniform', loc=loc, scale=scale)
+
+
+def normal(mu, sigma):
+    return ContinuousDomain('normal', loc=mu, scale=sigma)
