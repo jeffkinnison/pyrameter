81f3dce77257d35fb313c29ae0c2ce8d36c09f2f
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-03-03 08:59:00 -0600

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyrameter',
-    version='0.1.1post5',
+    version='0.2.0',
     description='Structure, sample, and savor hyperparameter searches',
     url='https://github.com/jeffkinnison/pyrameter',
     author='Jeff Kinnison',
