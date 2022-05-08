0b2f0f197e85f9baa35c950ce70c46260bbf45ec
sabraha2 <54291729+sabraha2@users.noreply.github.com>
2022-05-07 23:26:00 -0400

    Update __init__.py
    Extend to include ncqs

--- pyrameter/methods/__init__.py
+++ pyrameter/methods/__init__.py
@@ -3,6 +3,7 @@ from .pso import PSO as pso
 from .random_search import RandomSearch as random
 from .smac import SMAC as smac
 from .tpe import TPE as tpe
+from .ncqs import NCQS as ncqs
 
 
-__all__ = ['bayes', 'pso', 'random', 'smac', 'tpe']
+__all__ = ['bayes', 'pso', 'random', 'smac', 'tpe', 'ncqs']
